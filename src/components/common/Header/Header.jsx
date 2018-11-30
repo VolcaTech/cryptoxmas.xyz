435d2bb876abcf422a0ef5b5678d1f52c6d488b7
Alexander Milhaylov <sashamihaha@gmail.com>
2018-11-30 14:10:02 +0200

    new header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -10,8 +10,8 @@ class Header extends React.Component {
               <Row style={{ paddingTop: '30px', textAlign: 'center' }}>
 		<a href="/" className="no-underline">
                   <RetinaImage className="img-responsive" style={{ display: 'inline'}} src="https://raw.githubusercontent.com/VolcaTech/eth2-assets/master/images/wreath.png" />
-                  <div style={{display: 'inline', marginLeft: 18.5, fontFamily: 'Inter UI Bold', fontSize: 30, color: 'white', textAlign: 'right' }}>
-                    Crypto Christmas
+                  <div style={{display: 'inline', marginLeft: 87.5, fontFamily: 'Inter UI Bold', fontSize: 30, color: 'white', textAlign: 'right' }}>
+                    Crypto Xmas
                   </div>
 		</a>
 	      </Row>
