5886780b5b0fb4b86b41bd827fd25774a547636d
Alexander Milhaylov <sashamihaha@gmail.com>
2018-11-30 14:31:33 +0200

    link is claimed screen

--- src/components/common/ButtonPrimary.jsx
+++ src/components/common/ButtonPrimary.jsx
@@ -9,17 +9,17 @@ class e2pButtonPrimary extends React.Component {
             <Button className="button-primary" disabled={this.props.disabled} style={{
                 display: 'block',
                 margin: 'auto',
-			width: 300,
-			height: 50,
-			borderRadius: 12,
-			borderColor: '#4CD964',                
-			backgroundColor: '#4CD964',
-			opacity: this.props.disabled ? 0.5 : 1,
-			display: 'block', 
-            color: "#fff",
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-			fontSize: this.props.fontSize ? this.props.fontSize : 20,
-			fontFamily: "Inter UI Medium",                 
+                width: 300,
+                height: 50,
+                borderRadius: 12,
+                borderColor: '#4CD964',
+                backgroundColor: '#4CD964',
+                opacity: this.props.disabled ? 0.5 : 1,
+                display: 'block',
+                color: "#fff",
+                boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                fontSize: this.props.fontSize ? this.props.fontSize : 20,
+                fontFamily: "Inter UI Medium",
             }} onClick={this.props.handleClick}>
                 {this.props.children}
             </Button>
