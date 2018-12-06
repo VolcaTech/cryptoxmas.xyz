16e62ab8d98f44b7b88ba16adb9058e7866ce07c
Alexander Milhaylov <sashamihaha@gmail.com>
2018-12-07 00:53:08 +0200

    no wallet screens refactored

---
+++ src/components/NotConnectedScreens/styles.jsx
@@ -0,0 +1,122 @@
+export default {
+  unsupportedNetworkScreenTitle: {
+    width: "90%",
+    display: "block",
+    margin: "auto",
+    fontSize: 30,
+    lineHeight: 1,
+    fontFamily: "Inter UI Medium",
+    color: "#4CD964",
+    textAlign: "center",
+    marginBottom: 25,
+    marginTop: 50
+  },
+  unsupportedNetworkScreenText: {
+    fontFamily: "Inter UI Regular",
+    fontSize: 24
+  },
+  unsupportedNetworkScreenContainer: {
+    width: 400,
+    padding: 20,
+    margin: "auto",
+    textAlign: "left",
+    verticalAlign: "text-top",
+    marginTop: 40,
+    marginBottom: 35
+  },
+  walletSliderLogo: {
+    height: 60,
+    width: 60,
+    margin: 7,
+    borderRadius: 15,
+    WebkitBoxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)"
+  },
+  walletSliderLogo5: {
+    height: 50,
+    width: 50,
+    margin: 7,
+    borderRadius: 12.5,
+    WebkitBoxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)"
+  },
+  walletSliderTitle: {
+    textAlign: "center",
+    fontFamily: "Inter UI Regular",
+    fontSize: 18,
+    color: "#888888",
+    marginBottom: 20,
+    marginTop: 20
+  },
+  walletSliderNextArrow: {
+    width: 20,
+    height: 20,
+    position: "absolute",
+    top: 25,
+    right: -8,
+    textAlign: "right",
+    color: "#4CD964"
+  },
+  walletSliderPrevArrow: {
+    height: 20,
+    float: "left",
+    top: 25,
+    paddingTop: 26
+  },
+  noWalletScreenButton: {
+    display: "flex",
+    margin: "auto",
+    flexDirection: "column",
+    alignContent: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 50,
+    borderRadius: 10,
+    marginTop: "auto",
+    marginBottom: "auto",
+    backgroundColor: "#4CD964",
+    borderColor: "#4CD964",
+    WebkitBoxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    fontSize: 20,
+    fontFamily: "Inter UI Medium",
+    textAlign: "center",
+    textDecoration: "none",
+    color: "white"
+  },
+  noWalletInstructionText: {
+    fontFamily: "Inter UI Regular",
+    fontSize: 16,
+    color: "white",
+    marginBottom: 5
+  },
+  noWalletinstructionContainer: {
+    width: 300,
+    display: "flex",
+    margin: "auto",
+    textAlign: "left",
+    verticalAlign: "text-top",
+    marginTop: 25,
+    marginBottom: 25,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  largeWalletIcon: {
+    display: "block",
+    margin: "auto",
+    borderRadius: 25,
+    WebkitBoxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)",
+    marginTop: 50,
+    marginBottom: 30
+  },
+  screenContainer: {
+    width: 354,
+    margin: "auto",
+    marginTop: 50,
+    textAlign: "left"
+  },
+  greenTitle: {
+    marginBottom: 45,
+    fontFamily: "Inter UI Medium",
+    fontSize: 30,
+    color: "#4CD964",
+    textAlign: "left"
+  }
+};
