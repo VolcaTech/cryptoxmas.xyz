0e9e7efb418f3b651756266e848028618f0f2769
Alexander Milhaylov <sashamihaha@gmail.com>
2018-12-07 01:36:40 +0200

    receive screens refactored

---
+++ src/components/Receive/styles.jsx
@@ -0,0 +1,32 @@
+export default {
+  textContainer: {
+    width: 354,
+    margin: "auto",
+    marginTop: 50,
+    textAlign: "left"
+  },
+  greenTitle: {
+    fontFamily: "Inter UI Medium",
+    fontSize: 30,
+    color: "#4CD964",
+    textAlign: "left"
+  },
+  whiteTitle: {
+    marginBottom: 55,
+    fontFamily: "Inter UI Medium",
+    fontSize: 24,
+    color: "white",
+    textAlign: "left"
+  },
+  gifContainer: {
+    display: "block",
+    margin: "auto",
+    height: 200,
+    width: 200
+  },
+
+  button: {
+    margin: "auto",
+    marginTop: 40
+  }
+};
