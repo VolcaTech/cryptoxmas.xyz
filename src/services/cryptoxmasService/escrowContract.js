4276e728651d6a17d073cfd47991207c10fc396e
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-12-06 16:32:20 +0300

    escrow contract updated

--- src/services/cryptoxmasService/escrowContract.js
+++ src/services/cryptoxmasService/escrowContract.js
@@ -1,5 +1,5 @@
 import Promise from "bluebird";
-import Eth2gift from "../../../cryptoxmas-contracts/build/eth2giftEscrow.json";
+import Eth2gift from "../../../cryptoxmas-contracts/build/cryptoxmasEscrow.json";
 import config from "../../../dapp-config.json";
 
 class EscrowContractService {
