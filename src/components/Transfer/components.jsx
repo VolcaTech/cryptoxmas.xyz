99caddb82a106136550faa66e3d9135cb577999e
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-12-08 16:32:50 +0300

    hardcoded host removed

--- src/components/Transfer/components.jsx
+++ src/components/Transfer/components.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import copy from "copy-to-clipboard";
 import ButtonPrimary from "./../common/ButtonPrimary";
-const HOST = "https://app.cryptoxmas.xyz";
 const shareIcon = require("../../../public/images/share_icon.png");
 import { getCurrentWalletId } from "../../utils";
 import web3Service from "./../../services/web3Service";
@@ -18,8 +17,8 @@ export const getEtherscanLink = ({ txHash, networkId }) => {
 
 export const ShareButton = ({ transfer }) => {
   let shareLink;
-
-  shareLink = `${HOST}/#/r?pk=${transfer.transitPrivateKey}`;
+    const host = `${window.location.protocol}//${window.location.host}`;
+    shareLink = `${host}/#/r?pk=${transfer.transitPrivateKey}`;
 
   if (transfer.networkId !== "1") {
     shareLink += `&n=${transfer.networkId}`;
