90a29e83eaa48cabe65dc38f0587b51c8775adda
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-12-09 18:42:50 +0300

    comments for the Escrow Contract added

--- cryptoxmas-contracts/contracts/CryptoxmasEscrow.sol
+++ cryptoxmas-contracts/contracts/CryptoxmasEscrow.sol
@@ -9,11 +9,11 @@ import './NFT.sol';
 contract CryptoxmasEscrow is Pausable, Ownable {
   using SafeMath for uint256;
   
-  /* Giveth  */
+  /* Giveth */
   address public givethBridge;
   uint64 public givethReceiverId;
 
-  /* NFT   */
+  /* NFT */
   NFT public nft; 
   
   // commission to fund paying gas for claim transactions
@@ -26,13 +26,13 @@ contract CryptoxmasEscrow is Pausable, Ownable {
   
   struct Gift {
     address sender;
-    uint claimEth; // eth for receiver    
+    uint claimEth; // ETH for receiver    
     uint256 tokenId;
     Statuses status;
-    string msgHash; // ifps message hash
+    string msgHash; // IFPS message hash
   }
 
-  // Mappings of transitAddress => Transfer Struct
+  // Mappings of transitAddress => Gift Struct
   mapping (address => Gift) gifts;
 
 
@@ -74,29 +74,59 @@ contract CryptoxmasEscrow is Pausable, Ownable {
 		  uint tokenId
 		  );
   
-  
+
+  /**
+   * @dev Contructor that sets msg.sender as owner in Ownable,
+   * sets escrow contract params and deploys new NFT contract 
+   * for minting and selling tokens.
+   *
+   * @param _givethBridge address Address of GivethBridge contract
+   * @param _givethReceiverId uint64 Campaign Id created on Giveth platform.
+   * @param _name string Name for the NFT 
+   * @param _symbol string Symbol for the NFT 
+   */
   constructor(address _givethBridge,
 	      uint64 _givethReceiverId,
 	      string _name,
 	      string _symbol) public {
+    // setup Giveth params
     givethBridge = _givethBridge;
     givethReceiverId = _givethReceiverId;
-
+    
+    // deploy nft contract
     nft = new NFT(_name, _symbol);
   }
 
+   /* 
+   * METHODS 
+   */
+  
+  /**
+   * @dev Get Token Category for the tokenUri.
+   *
+   * @param _tokenUri string token URI of the category
+   * @return Token Category details (CategoryId, minted, maxQnty, price)
+   */  
   function getTokenCategory(string _tokenUri) public view returns (CategoryId categoryId,
 								  uint minted,
 								  uint maxQnty,
 								  uint price) { 
-    TokenCategory memory category = tokenCategories[_tokenUri];
-    
+    TokenCategory memory category = tokenCategories[_tokenUri];    
     return (category.categoryId,
 	    category.minted,
 	    category.maxQnty,
 	    category.price);
   }
 
+  /**
+   * @dev Add Token Category for the tokenUri.
+   *
+   * @param _tokenUri string token URI of the category
+   * @param _categoryId uint categoryid of the category
+   * @param _maxQnty uint maximum quantity of tokens allowed to be minted
+   * @param _price uint price tokens of that category will be sold at  
+   * @return True if success.
+   */    
   function addTokenCategory(string _tokenUri, CategoryId _categoryId, uint _maxQnty, uint _price)
     public onlyOwner returns (bool success) {
 
@@ -113,7 +143,14 @@ contract CryptoxmasEscrow is Pausable, Ownable {
     return true;
   }
 
-  
+  /**
+   * @dev Checks that it's possible to buy gift and mint token with the tokenURI.
+   *
+   * @param _tokenUri string token URI of the category
+   * @param _transitAddress address transit address assigned to gift
+   * @param _value uint amount of ether, that is send in tx. 
+   * @return True if success.
+   */      
   function canBuyGift(string _tokenUri, address _transitAddress, uint _value) public view returns (bool) {
     // can not override existing gift
     require(gifts[_transitAddress].status == Statuses.Empty);
@@ -122,26 +159,38 @@ contract CryptoxmasEscrow is Pausable, Ownable {
     TokenCategory memory category = tokenCategories[_tokenUri];
     require(_value >= category.price);
 
-    // can not override existing gift
-    require(gifts[_transitAddress].status == Statuses.Empty);
-
     // tokens of that type not sold out yet
     require(category.minted < category.maxQnty);
     
     return true;
   }
 
-
+  /**
+   * @dev Buy gift and mint token with _tokenUri, new minted token will be kept in escrow
+   * until receiver claims it. 
+   *
+   * Received ether, splitted in 3 parts:
+   *   - 0.01 ETH goes to ephemeral account, so it can pay gas fee for claim transaction. 
+   *   - token price (minus ephemeral account fee) goes to the Giveth Campaign as a donation.  
+   *   - Eth above token price is kept in the escrow, waiting for receiver to claim. 
+   *
+   * @param _tokenUri string token URI of the category
+   * @param _transitAddress address transit address assigned to gift
+   * @param _msgHash string IPFS hash, where gift message stored at 
+   * @return True if success.
+   */    
   function buyGift(string _tokenUri, address _transitAddress, string _msgHash)
           payable public whenNotPaused returns (bool) {
     
     require(canBuyGift(_tokenUri, _transitAddress, msg.value));
 
+    // get token price from the category for that token URI
     uint tokenPrice = tokenCategories[_tokenUri].price;
+
+    // ether above token price is for receiver to claim
     uint claimEth = msg.value.sub(tokenPrice);
 
-
-    // mint nft 
+    // mint new token 
     uint tokenId = tokensCounter.add(1);
     nft.mintWithTokenURI(tokenId, _tokenUri);
 
@@ -159,9 +208,10 @@ contract CryptoxmasEscrow is Pausable, Ownable {
 				  );
 
 
-    // transfer ETH to relayer to fund claim txs
+    // transfer small fee to ephemeral account to fund claim txs
     _transitAddress.transfer(EPHEMERAL_ADDRESS_FEE);
-    
+
+    // send donation to Giveth campaign
     uint donation = tokenPrice.sub(EPHEMERAL_ADDRESS_FEE);
     if (donation > 0) {
       _makeDonation(msg.sender, donation);
@@ -178,7 +228,14 @@ contract CryptoxmasEscrow is Pausable, Ownable {
     return true;
   }
 
-
+  /**
+   * @dev Send donation to Giveth campaign 
+   * by calling function 'donateAndCreateGiver' of GivethBridge contract.
+   *
+   * @param _giver address giver address
+   * @param _value uint donation amount (in wei)
+   * @return True if success.
+   */    
   function _makeDonation(address _giver, uint _value) internal returns (bool success) {
     bytes memory _data = abi.encodePacked(0x1870c10f, // function signature
 					   bytes32(_giver),
@@ -190,19 +247,24 @@ contract CryptoxmasEscrow is Pausable, Ownable {
     return success;
   }
 
-  
+  /**
+   * @dev Get Gift assigned to transit address.
+   *
+   * @param _transitAddress address transit address assigned to gift
+   * @return Gift details
+   */    
   function getGift(address _transitAddress) public view returns (
 	     uint256 tokenId,
 	     string tokenUri,								 
-	     address sender, // transfer sender
-	     uint claimEth,
-	     uint nftPrice,	     
-	     Statuses status,								 	     
-	     string msgHash) {
+	     address sender,  // gift buyer
+	     uint claimEth,   // eth for receiver
+	     uint nftPrice,   // token price 	     
+	     Statuses status, // gift status (deposited, claimed, cancelled) 								 	     
+	     string msgHash   // IPFS hash, where gift message stored at 
+    ) {
     Gift memory gift = gifts[_transitAddress];
     tokenUri =  nft.tokenURI(gift.tokenId);
-    TokenCategory memory category = tokenCategories[tokenUri];
-    
+    TokenCategory memory category = tokenCategories[tokenUri];    
     return (
 	    gift.tokenId,
 	    tokenUri,
@@ -213,11 +275,11 @@ contract CryptoxmasEscrow is Pausable, Ownable {
 	    gift.msgHash
 	    );
   }
-
   
   /**
    * @dev Cancel gift and get sent ether back. Only gift buyer can
    * cancel.
+   * 
    * @param _transitAddress transit address assigned to gift
    * @return True if success.
    */
@@ -250,7 +312,7 @@ contract CryptoxmasEscrow is Pausable, Ownable {
    * @return True if success.
    */
   function claimGift(address _receiver) public whenNotPaused returns (bool success) {
-
+    // only holder of ephemeral private key can claim gift
     address _transitAddress = msg.sender;
     
     Gift storage gift = gifts[_transitAddress];
@@ -258,10 +320,10 @@ contract CryptoxmasEscrow is Pausable, Ownable {
     // is deposited and wasn't claimed or cancelled before
     require(gift.status == Statuses.Deposited);
 
-    /* // update gift status */
+    // update gift status to claimed
     gift.status = Statuses.Claimed;
     
-    // send nft
+    // send nft to receiver
     nft.transferFrom(address(this), _receiver, gift.tokenId);
     
     // transfer ether to receiver's address
@@ -269,7 +331,7 @@ contract CryptoxmasEscrow is Pausable, Ownable {
       _receiver.transfer(gift.claimEth);
     }
 
-    // log withdraw event
+    // log claim event
     emit LogClaim(_transitAddress, gift.sender, gift.tokenId, _receiver, gift.claimEth);
     
     return true;
