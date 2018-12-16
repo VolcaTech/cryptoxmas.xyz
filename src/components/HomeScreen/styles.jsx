cd52ce4405ea6ad50602bd0de311958458c72def
Alexander Milhaylov <sashamihaha@gmail.com>
2018-12-16 04:15:35 +0200

    home screen updated

--- src/components/HomeScreen/styles.jsx
+++ src/components/HomeScreen/styles.jsx
@@ -2,8 +2,8 @@ export default {
   nftContainer: {
     display: "flex",
     flexDirection: "column",
-      alignItems: "center",
-      margin:'auto',
+    alignItems: "center",
+    margin: "auto",
     width: 170,
     height: 170,
     backgroundColor: "white",
@@ -16,12 +16,29 @@ export default {
     marginBottom: 30
   },
   nftPrice: {
-    width: "100%",
-    margin: "8px 25px 0px 0px",
     textAlign: "right",
     color: "#4CD964",
     fontFamily: "Inter UI Bold",
     fontSize: 14
+  },
+  nftPriceContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px 10px 0px 10px"
+  },
+  nftRarity: {
+    display: "inline",
+    textAlign: "center",
+    fontFamily: "Inter UI Medium",
+    fontSize: 11,
+    textTransform: "capitalize",
+    padding: "3px 7px",
+    borderRadius: 10,
+    backgroundColor: "#4CD964",
+    color: "white"
   },
   nftImage: {
     display: "block",
@@ -32,61 +49,83 @@ export default {
     textAlign: "center",
     color: "black",
     fontFamily: "Inter UI Bold",
-      fontSize: 14,
-      height: 20,
-      overflow: 'hidden'
+    fontSize: 14,
+    height: 20,
+    overflow: "hidden"
   },
   homescreenContainer: {
-      width: "100%",
-      backgroundColor: "#474D5B",
-      backgroundImage:
+    width: "100%",
+    backgroundColor: "#474D5B",
+    backgroundImage:
       "url(https://raw.githubusercontent.com/VolcaTech/eth2-assets/master/images/sparkles_tree.png)",
-      backgroundPosition: "right top",
-      backgroundRepeat: "no-repeat",
-      //
+    backgroundPosition: "right top",
+    backgroundRepeat: "no-repeat"
+    //
   },
   homescreenContent: {
-      maxWidth: 1000,
-      margin: 'auto',
-      height: window.innerHeight,
-  },    
+    maxWidth: 1000,
+    margin: "auto",
+    height: window.innerHeight
+  },
   homescreenTextContainer: {
-      width: (window.innerWidth > 667 ? '100%': Math.min(354, window.innerWidth)),
-      margin: "auto",
-      marginTop: 50,
-      textAlign: "left",      
+    width: window.innerWidth > 667 ? 528 : Math.min(354, window.innerWidth),
+    margin: "auto",
+    marginLeft: window.innerWidth > 667 ? "auto" : 20,
+    marginTop: 50,
+    textAlign: "left"
   },
   homescreenGreenTitle: {
     marginBottom: 25,
     fontFamily: "Inter UI Medium",
     fontSize: 30,
     color: "#4CD964",
-      textAlign: (window.innerWidth > 667 ? 'center': 'left'),
+    textAlign: "left"
   },
-  homescreenGreyText: {
-      marginBottom: 50,
-      fontFamily: "Inter UI Regular",
-      fontSize: 18,
-      color: "#8B8B8B",
-      textAlign: (window.innerWidth > 667 ? 'center': 'left'),
+  homescreenWhiteText: {
+    marginLeft: 52,
+    marginBottom: 50,
+    width: window.innerWidth > 667 ? 402 : 237,
+    fontFamily: "Inter UI Regular",
+    fontSize: 18,
+    color: "white",
+    textAlign: window.innerWidth > 667 ? "center" : "left"
   },
-  homescreenWhiteTitle: {
-      marginBottom: 20,
-      fontFamily: "Inter UI Medium",
-      fontSize: 24,
-      color: "white",
-      paddingLeft: (window.innerWidth > 667 ? 40: 0),
+  greenContainer: {
+    width: 250,
+    height: 40,
+    textAlign: "center",
+    backgroundColor: "#4CD964",
+    margin: "auto",
+    marginBottom: 20,
+    borderRadius: 20,
+    fontFamily: "Inter UI Regular",
+    fontSize: 20,
+    color: "white",
+    paddingTop: 5
   },
-    groupTitle: {
-	margin: "auto",
-	marginBottom: 30,
-	fontFamily: "Inter UI Medium",
-	fontSize: 24,
-	textAlign: "left",
-	marginBottom: 25,
-	width: (window.innerWidth > 667 ? '100%': Math.min(354, window.innerWidth)),
-	paddingLeft: (window.innerWidth > 667 ? 25: 0),
-	color: "#FFF100"  
-    },
-
+  groupTitle: {
+    margin: "auto",
+    marginBottom: 30,
+    fontFamily: "Inter UI Medium",
+    fontSize: 24,
+    textAlign: "left",
+    marginBottom: 25,
+    width: window.innerWidth > 667 ? "100%" : Math.min(354, window.innerWidth),
+    paddingLeft: window.innerWidth > 667 ? 25 : 0,
+    color: "#FFF100"
+  },
+  redDot: {
+    display: "inline-block",
+    fontSize: 20,
+    verticalAlign: "text-bottom",
+    paddingTop: 2,
+    marginTop: 6,
+    marginRight: 20,
+    textAlign: "center",
+    color: "white",
+    width: 32,
+    height: 32,
+    backgroundColor: "#D9544C",
+    borderRadius: 16
+  }
 };
--- src/components/common/QuestionButton.jsx
+++ src/components/common/QuestionButton.jsx
@@ -7,17 +7,18 @@ class QuestionButton extends React.Component {
 	   className="hover"
         style={{
           display: "inline-block",
-          width: 25,
-          height: 25,
+          width: this.props.width || 25,
+          height: this.props.height || 25,
           borderRadius: 12.5,
           boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-          color: "#474D5B",
-          backgroundColor: "#8B8B8B",
+          color: this.props.color || "#474D5B",
+          backgroundColor: this.props.backgroundColor || "#8B8B8B",
           textAlign: "center",
-          fontFamily: "SF Display Bold",
-          fontSize: 16,
-          lineHeight: "26px",
-          marginLeft: 5
+          fontFamily: "Inter UI Regular",
+          fontSize: this.props.fontSize || 16,
+          lineHeight: this.props.lineHeight || "26px",
+          marginLeft: this.props.marginLeft || 5,
+          verticalAlign: "bottom"
         }}
       >
         ?
